31940de4542c1725d6cb3e6940d9e4f1d500bfeb
Alexandre Fenyo <fenyo@mac-mini-de-alexandre.local>
2018-03-26 18:13:54 +0200

    update

--- test1/ViewController.swift
+++ test1/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import CoreFoundation
 
 class ViewController: UIViewController {
 
     //MARK: Properties
     
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var console: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,8 +28,37 @@ class ViewController: UIViewController {
 
     //MARK: Actions
     
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+    @IBAction func bouton(_ sender: UIButton) {
+        // nameTextField.text = "salut";
+        console.text = console.text + "bouton(_:) called\n";
     }
     
+    @IBAction func action1(_ sender: UIButton) {
+        print("action1")
+    }
+    
+    @IBAction func action2(_ sender: UIButton) {
+        func cb(_ s : CFSocket?, _ type : CFSocketCallBackType, _data : CFData?, _p1 : UnsafeRawPointer?, _p2 : UnsafeMutableRawPointer?) {
+            print("Socket callback called")
+        }
+        
+        let s : CFSocket! = CFSocketCreate(nil, PF_INET, SOCK_STREAM, IPPROTO_TCP, CFSocketCallBackType.acceptCallBack.rawValue, cb, nil)
+        if let _ = s { print("CFSocketCreate returned OK") }
+
+        var sin = sockaddr_in(
+            sin_len: UInt8(MemoryLayout<sockaddr_in>.size),
+            sin_family: sa_family_t(AF_INET),
+            sin_port: in_port_t(8080),
+            sin_addr: in_addr(s_addr: INADDR_ANY),
+            sin_zero: (0,0,0,0,0,0,0,0)
+        )
+
+        let data = NSData(bytes: &sin, length: MemoryLayout<sockaddr_in>.size) as CFData
+        let cfSocketError : CFSocketError = CFSocketSetAddress(s, data)
+
+    }
+    
+    @IBAction func action3(_ sender: UIButton) {
+    }
 }
 
